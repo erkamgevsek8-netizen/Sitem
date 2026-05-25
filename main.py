7fbd4bc5db82e4cee37afd2951421cf677ce37f0
erkamgevsek8-netizen <erkamgevsek8@gmail.com>
2026-05-25 14:44:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+ from  flask import Flask
 
 app = Flask(__name__)
 
@@ -55,3 +55,6 @@ def ana_sayfa():
         </body>
     </html>
     """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
